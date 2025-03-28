3fc93ec418a5f50029f5f864af46fe2d141e854c
OmarRdz12 <martin.rojas@encora.com>
2025-03-27 20:54:02 -0600

    feat(frontend): create context to search form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import SearchPanel from "./components/SearchPanel"
+import GlobalContextProvider from "./context/GlobalContext"
 
 function App() {
   return (
-    <>
+    <GlobalContextProvider>
       <div className="w-screen h-screen flex flex-col items-center p-4 gap-4">
         <SearchPanel />
       </div>
-    </>
+    </GlobalContextProvider>
   )
 }
 
---
+++ frontend/src/context/GlobalContext.tsx
@@ -0,0 +1,19 @@
+import React, { createContext, useState } from "react"
+import { SearchForm } from "../interfaces/types"
+
+export const GlobalContext = createContext<any>(null)
+
+const GlobalContextProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const [searchForm, setSearchForm] = useState<SearchForm>({ originLocationCode: "", destinationCode: "", departureDate: "", adults: 1, nonStop: false })
+
+    return (
+        <GlobalContext.Provider value={{
+            searchForm,
+            setSearchForm
+        }}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
+
+export default GlobalContextProvider
